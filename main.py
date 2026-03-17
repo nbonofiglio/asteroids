93b74969c22a5aa3f225eca3b75efad97d23141b
Nick Bonofiglio <nick@Nicks-MacBook-Air.local>
2026-03-17 15:41:15 -0400

    added player sprite

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import pygame
-import constants
+import player
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
 
 def main():
     # startup messages
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     
-    # initializing game and window
+    # initializing game, window, and player sprite
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_sprite = player.Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
     # setting frames-per-second cap along with .tick() below
     clock = pygame.time.Clock()
@@ -22,6 +24,9 @@ def main():
         for event in pygame.event.get():
             pass
         screen.fill("black")
+        print(player_sprite.position, player_sprite.radius, player_sprite.rotation)
+        triangle_sprite = player_sprite.draw(screen)
+        print(triangle_sprite)
         pygame.display.flip()
 
         # lock sceen to 60 FPS
