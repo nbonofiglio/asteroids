3d9cd43237dc5570bac94df6dd91f99d14677f23
Nick Bonofiglio <nick@Nicks-MacBook-Air.local>
2026-03-17 13:47:26 -0400

    added lock to 60 FPS

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ import constants
 from logger import log_state
 
 def main():
+    # startup messages
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     
+    # initializing game and window
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    
+    # setting frames-per-second cap along with .tick() below
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # update game state for each frame
     while True:
         log_state()
         for event in pygame.event.get():
             pass
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
+        
+
 
 if __name__ == "__main__":
     main()
